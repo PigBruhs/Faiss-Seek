e8a0a29f49d67d0554e60d2427ec908e41005d74
Seregon the Mage <seregonzhang@qq.com>
2025-06-03 10:27:39 +0000

    修复了后端的bug

--- server/imageService.py
+++ server/imageService.py
@@ -21,51 +21,22 @@ class ImageService:
 
 
     def img_search(self,img,model="vgg16",top_n=5):
-        return searcher.search_topn(img, model=model, top_n=top_n, fe=self.fe)
+        try:
+            topn = searcher.search_topn(img, model=model, top_n=top_n, fe=self.fe)
+        except Exception as e:
+            print("图片匹配失败")
+            result = {"success": False, "message": "图片匹配失败"}
+            return result
 
-if __name__=="__main__":
-    # 测试 ImageService 类
-    image_service = ImageService()
-    test_image_path = "../data/search/002_anchor_image_0001.jpg"
-    results = image_service.img_search(test_image_path, model="vit16", top_n=5)
-    print(results)
+        base_url = request.host_url + "data/base/"
+        results = [{"name": name, "url": base_url + name, "score": score} for name, score in topn]
 
-
-imageService=ImageService()
-
-def ImageMatch(file):
-    filename = secure_filename(file.filename)
-    if '.' not in filename:
-        filename += '.jpg'
-
-    temp_dir = '../data/temp'
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-    file_path = os.path.join(temp_dir, filename)
-    file.save(file_path)
-
-    if not os.path.exists(file_path):
-        result={"success":False,"message":"文件保存失败"}
-        return result
-
-    try:
-        topn = imageService.img_search(file_path, model="vgg16", top_n=5)
-    except Exception as e:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        print("图片匹配失败")
-        result={"success":False,"message":"图片匹配失败"}
+        result = {"success": True, "message": "图片接受成功", "result": results}
         return result
 
 
-    if os.path.exists(file_path):
-        os.remove(file_path)
 
-    base_url = request.host_url + "data/base/"
-    results = [{"name": name, "url": base_url + name, "score": score} for name, score in topn]
 
-    result={"success":True, "message":"图片接受成功", "result":results}
-    return result
 
 
 
